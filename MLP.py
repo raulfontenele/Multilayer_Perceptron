2afbcf8663005330683e42970b03bc28c8e92432
Raul Fontenele <raulfontenele@edu.unifor.br>
2020-07-25 11:34:32 -0300

    Introdução do histórico de erro quadrático médio e correção na normalização dos dados

--- MLP.py
+++ MLP.py
@@ -2,19 +2,22 @@ import numpy as np
 import math
 
 class MLP():
-    def __init__(self,n_epoch,learn_rate,precision,hidden_layers):
+    def __init__(self,n_epoch,learn_rate,precision,hidden_layers,margin = 1000):
         self.n_epoch = n_epoch
         self.learn_rate = learn_rate
         self.precision = precision
         self.hidden_layers = hidden_layers
         self.weights = []
+        self.limits = []
+        self.margin = margin
 
     def train(self,x_train,d_train):
         #Inicialização de variáveis da rede neural
         I,Y,gradient,self.weights,accumulator = self.init_variables(len(x_train[0,:]),len(d_train[0,:]))
-        x_train = self.preparateInputData(x_train)
+        x_train = self.preparateInputData(x_train,self.margin,'train')
         eqm = [0, self.precision+1]
         epoch = 0
+        histEqm = []
 
         while True:
 
@@ -29,7 +32,7 @@ class MLP():
                 # Foward
                 for ctr in range( len(self.hidden_layers) + 1 ):
                     if ctr == 0:
-                        I[ctr] = np.dot(self.weights[ctr],x_train[sample,:][np.newaxis].transpose()) 
+                        I[ctr] = np.dot(self.weights[ctr] , x_train[sample,:][np.newaxis].transpose())
                         Y[ctr] = np.concatenate( (-1*np.ones((1,1)), np.tanh(I[ctr])) )
                     elif ctr == len(self.hidden_layers):
                         I[ctr] = np.dot(self.weights[ctr],Y[ctr-1])
@@ -45,9 +48,9 @@ class MLP():
                         accumulator[ctr] += self.learn_rate * np.dot(gradient[ctr],x_train[sample,:][np.newaxis])
                     elif ctr == len(self.hidden_layers):
                         gradient[ctr] = (d_train[sample,:][np.newaxis].transpose() - Y[ctr]) * self.diff_tanh(I[ctr])
-                        accumulator[ctr] += self.learn_rate * np.dot(gradient[ctr],Y[ctr-1].transpose())
+                        accumulator[ctr] += self.learn_rate * (gradient[ctr]@Y[ctr-1].transpose())
                     else:
-                        gradient[ctr] = np.dot(self.weights[ctr+1][:,1::].transpose(),gradient[ctr + 1]) * self.diff_tanh(I[ctr])
+                        gradient[ctr] = np.dot(self.weights[ctr+1][:,1::].transpose() , gradient[ctr + 1]) * self.diff_tanh(I[ctr])
                         accumulator[ctr] += self.learn_rate * np.dot(gradient[ctr],Y[ctr - 1].transpose())
 
             # Ajuste dos pesos da rede neural
@@ -68,15 +71,16 @@ class MLP():
                         I[ctr] = np.dot(self.weights[ctr],Y[ctr-1])
                         Y[ctr] = np.concatenate( (-1*np.ones((1,1)), np.tanh(I[ctr])) )
 
-                squareError += sum((d_train[sample,:][np.newaxis].transpose() - Y[len(self.hidden_layers)])**2)
+                squareError += sum(( d_train[sample,:][np.newaxis].transpose() - Y[len(self.hidden_layers)] )**2)
             
             eqm[1] = squareError/len(x_train)
-                
-            epoch+=1
-            print(abs(eqm[1] - eqm[0]))
+            histEqm.append(eqm[1])
 
             if(abs(eqm[1] - eqm[0])<=self.precision or epoch>self.n_epoch):
-                break
+                return [epoch,histEqm]
+
+            epoch+=1
+
 
     def application(self,x_app):
         #Inicialização de variáveis
@@ -85,6 +89,9 @@ class MLP():
         Y = variables[1]
 
         result = []
+
+        #Normalização dos dados de entrada
+        x_app = self.preparateInputData(x_app,self.margin,'app')
 
         for sample in range( len(x_app) ):
             # Foward
@@ -141,24 +148,38 @@ class MLP():
         return I,Y,gradient,weights,accumulator
             
     def realizeProbability(self, vector):
+        result = []
         for index in range(len(vector)):
             if vector[index] == max(vector):
-                vector[index] = 1
+                result.append(1)
             else:
-                vector[index] = -1
-
-        return vector
+                result.append(-1)
+        
+        return result
 
     def diff_tanh(self,value):
         return (1 - np.tanh(value)**2)
     
-    def preparateInputData(self, inputData):
-        #Normalização dos dados
-        for column in range(len(inputData[0])):
-            inputData[:,column] = ( inputData[:,column] - min(inputData[:,column]) ) - ( max(inputData[:,column]) - min(inputData[:,column]) )
+    def preparateInputData(self, inputData, margin,application):
+        #Normalização dos dados e guardar os maiores e menores valores por coluna
+        new_inputData = self.normalizeData(inputData,margin,application)
 
         #Concatenar uma matriz coluna igual a -1
-        aux_matrix = -1 * np.ones( (len(inputData),1) )
-        inputData = np.concatenate((aux_matrix,inputData),axis=1)
+        aux_matrix = -1 * np.ones( (len(new_inputData),1) )
+        new_inputData = np.concatenate((aux_matrix,new_inputData),axis=1)
+
+        return new_inputData
+    
+    def normalizeData(self,inputData,margin,application):
+
+        if application == 'train':
+            #Normalização dos dados e guardar os maiores e menores valores por coluna
+            for column in range(len(inputData[0])):
+                self.limits.append([ max(inputData[:,column]), min(inputData[:,column] )])
+                inputData[:,column] = (( inputData[:,column] - (min(inputData[:,column])-margin) ) / ( max(inputData[:,column]) - min(inputData[:,column]) + 2*margin)) -0.5
+        else:
+            #Normalização dos dados e guardar os maiores e menores valores por coluna
+            for column in range(len(inputData[0])):
+                inputData[:,column] = ( inputData[:,column] - (self.limits[column][1]-margin) ) / ( self.limits[column][0] - self.limits[column][1] + 2*margin) -0.5
 
         return inputData
